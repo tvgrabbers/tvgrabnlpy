b21fe9cb7387fc3ac51a4c24dca1613c978bfef8
Hika van den Hoven <hikavdh@gmail.com>
2015-06-23 15:55:09 +0200

    Updated patchdate

--- tv_grab_nl.py
+++ tv_grab_nl.py
@@ -267,7 +267,7 @@ class Configure:
         self.major = 2
         self.minor = 1
         self.patch = 9
-        self.patchdate = u'20150621'
+        self.patchdate = u'20150622'
         self.alfa = False
         self.beta = True
 
