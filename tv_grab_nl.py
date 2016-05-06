a96406bc9225ffb43c604d6df182456008f7f458
Hika van den Hoven <hikavdh@gmail.com>
2016-05-06 20:38:25 +0200

    Set Stable

--- tv_grab_nl.py
+++ tv_grab_nl.py
@@ -359,9 +359,9 @@ class Configure:
         self.major = 2
         self.minor = 2
         self.patch = 14
-        self.patchdate = u'20160505'
+        self.patchdate = u'20160506'
         self.alfa = False
-        self.beta = True
+        self.beta = False
 
         self.cache_return = Queue()
         self.channels = {}
