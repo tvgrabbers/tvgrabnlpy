f1d6b9740b6fdf9f69d7d384d4ace9879a11986f
Hika van den Hoven <hikavdh@gmail.com>
2016-09-25 21:48:44 +0200

    Update patchdate

--- tv_grab_nl.py
+++ tv_grab_nl.py
@@ -427,9 +427,9 @@ class Configure:
         self.major = 2
         self.minor = 2
         self.patch = 19
-        self.patchdate = u'20160908'
+        self.patchdate = u'20160925'
         self.alfa = False
-        self.beta = True
+        self.beta = False
 
         self.cache_return = Queue()
         self.channels = {}
